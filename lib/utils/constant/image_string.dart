11db92aff8b64fb230e24ef65f53cd9dc550435a
Enes YILDIZ <yildizenes.4207@hotmail.com>
2024-05-27 13:15:08 +0300

    image string added

--- lib/utils/constant/image_string.dart
+++ lib/utils/constant/image_string.dart
@@ -1,1 +1,177 @@
-// Enes Yıldız - Zehra Karaca
+class ImageString {
+  //bottom navigation bar
+  static const String homepage = "assets/icons/anasayfa_inaktif.png";
+  static const String homepageActive = "assets/icons/anasayfa_aktif.png";
+  static const String trainings = "assets/icons/eğitimlerim_inaktif.png";
+  static const String trainingsActive = "assets/icons/eğitimlerim_aktif.png";
+  static const String catalog = "assets/icons/katalog_inaktif.png";
+  static const String catalogActive = "assets/icons/katalog_aktif.png";
+  static const String profile = "assets/icons/profil_inaktif.png";
+  static const String profileActive = "assets/icons/profil_aktif.png";
+  static const String calendar = "assets/icons/takvim_inaktif.png";
+  static const String calendarActive = "assets/icons/takvim_aktif.png";
+
+  //login
+  static const String splashScreenGif = "assets/gifs/splash_screen.gif";
+  static const String intro1 = "assets/images/intro_1.png";
+  static const String intro2 = "assets/images/intro_2.png";
+  static const String intro3 = "assets/images/intro_3.png";
+  static const String loginBackground = "assets/images/login_background.png";
+  static const String facebookIcon = "assets/icons/facebook_icon.png";
+  static const String googleIcon = "assets/icons/google_icon.png";
+  static const String visitorIcon = "assets/icons/misafir_giris.png";
+  static const String passwordIcon = "assets/icons/sifre.png";
+
+  static const String ikLogoDart = "assets/images/ik-logo-dark.png";
+  static const String ikLogoLight = "assets/images/ik-logo-light.png";
+
+  static const String ecmelAyral = "assets/images/ecmel_ayral.jpg";
+  static const String howToTraining = "assets/images/eğitimlere_nasil_katilirim.jpg";
+  static const String notFound = "assets/images/notFound.png";
+  static const String heart = "assets/icons/heart.png";
+  static const String timer = "assets/icons/timer.png";
+  static const String timerWhite = "assets/icons/timer_white.png";
+  static const String point = "assets/icons/puan_icon.png";
+  static const String pointWhite = "assets/icons/puan_white.png";
+  static const String language = "assets/icons/language.png";
+  static const String languageWhite = "assets/icons/language_white.png";
+
+  static const String organizer = "assets/icons/organizer.png";
+  static const String user = "assets/icons/user.png";
+  static const String userWhite = "assets/icons/user_white.png";
+  static const String gender = "assets/icons/gender.png";
+  static const String genderWhite = "assets/icons/gender_white.png";
+  static const String soldier = "assets/icons/soldier.png";
+  static const String soldierWhite = "assets/icons/soldier_white.png";
+  static const String personalInformation = "assets/icons/user_profile_edit.png";
+  static const String experiences = "assets/icons/suitcase_profile_edit.png";
+  static const String education = "assets/icons/profil_edit_read.png";
+  static const String competencies = "assets/icons/profile_edit_prize.png";
+  static const String certificates = "assets/icons/profile_edit_quality.png";
+  static const String communities = "assets/icons/profile_edit_documents.png";
+  static const String projectsAndAwards = "assets/icons/profile_edit_trophy.png";
+  static const String socialMedia = "assets/icons/profile_edit_longitude.png";
+  static const String foreignLanguages = "assets/icons/profile_edit_language.png";
+  static const String flagTurkey = "assets/icons/turkey.png";
+  static const String trash = "assets/icons/profile_edit_trash.png";
+  static const String profileEdit = "assets/icons/profile_edit_create.png";
+  static const String profileEditWhite = "assets/icons/profile_edit_create_white.png";
+  static const String profileEditNote = "assets/images/profile_edit_note.png";
+  static const String linkedIn = "assets/icons/linkedIn.png";
+  static const String platformIcon = "assets/icons/platform-icon.png";
+  static const String analysis = "assets/images/iste_basari_modeli_analiz.png";
+  static const String trueDark = "assets/images/true_dark.png";
+  static const String falseDark = "assets/images/false_dark.png";
+  static const String emptyDark = "assets/images/empty_dark.png";
+  static const String pointDark = "assets/images/point_dark.png";
+  static const String trueLight = "assets/images/true_light.png";
+  static const String falseLight = "assets/images/false_light.png";
+  static const String emptyLight = "assets/images/empty_light.png";
+  static const String pointLight = "assets/images/point_light.png";
+
+//Zehra
+  static const String profilePhoto = "assets/images/profile_photo.png";
+  static const String student = "assets/images/student.png";
+
+  static const String greyTobeto = "assets/images/grey-tobeto.png";
+  static const String whiteTobeto = "assets/images/white-tobeto.png";
+  static const String tTobeto = "assets/images/tobeto-t.png";
+
+  static const String enocta = "assets/images/enocta.png";
+  static const String advancity = "assets/images/advancity.png";
+  static const String codecademy = "assets/images/codecademy.png";
+  static const String perculus = "assets/images/perculus.png";
+  static const String huawei = "assets/images/huawei.png";
+  static const String stb = "assets/images/stb.png";
+  static const String istka = "assets/images/istka.png";
+  static const String bop = "assets/images/bop.png";
+  static const String etkiYap = "assets/images/etki-yap.png";
+
+  static const String yazilim = "assets/images/yazilim.png";
+  static const String whiteYazilim = "assets/images/white-yazilim.png";
+  static const String siber = "assets/images/siber.png";
+  static const String whiteSiber = "assets/images/white-siber.png";
+  static const String veri = "assets/images/veri.png";
+  static const String whiteVeri = "assets/images/white-veri.png";
+  static const String bulut = "assets/images/bulut.png";
+  static const String whiteBulut = "assets/images/white-bulut.png";
+  static const String mobil = "assets/images/mobil.png";
+  static const String whiteMobil = "assets/images/white-mobil.png";
+  static const String uiux = "assets/images/uiux.png";
+  static const String whiteUiux = "assets/images/white-uiux.png";
+  static const String dijital = "assets/images/dijital.png";
+  static const String whiteDijital = "assets/images/white-dijital.png";
+  static const String ag = "assets/images/ag.png";
+  static const String whiteAg = "assets/images/white-ag.png";
+  static const String sistem = "assets/images/sistem.png";
+  static const String whiteSistem = "assets/images/white-sistem.png";
+  static const String oyun = "assets/images/oyun.png";
+  static const String whiteOyun = "assets/images/white-oyun.png";
+
+  static const String ik1 = "assets/images/ik1.jpg";
+  static const String ik2 = "assets/images/ik2.jpg";
+  static const String ik3 = "assets/images/ik3.jpg";
+  static const String ik4 = "assets/images/ik4.jpg";
+
+  static const String elifKilic = "assets/images/elif-kilic.png";
+  static const String kaderYavuz = "assets/images/kader-yavuz.png";
+  static const String pelinBatir = "assets/images/pelin-batir.png";
+  static const String gurkanIlisen = "assets/images/gurkan-ilisen.png";
+  static const String aliSeyhan = "assets/images/ali-seyhan.png";
+
+  static const String basin1 = "assets/images/basindabiz1.jpg";
+  static const String basin2 = "assets/images/basindabiz2.jpg";
+  static const String basin3 = "assets/images/basindabiz3.jpg";
+  static const String basin4 = "assets/images/basindabiz4.jpg";
+  static const String basin5 = "assets/images/basindabiz5.png";
+
+  static const String biz1 = "assets/images/bizkimiz1.png";
+  static const String biz2 = "assets/images/bizkimiz2.png";
+  static const String biz3 = "assets/images/bizkimiz3.png";
+  static const String biz4 = "assets/images/bizkimiz4.png";
+  static const String code = "assets/images/code.png";
+  static const String ekip1 = "assets/images/ekip1.jpg";
+  static const String ekip2 = "assets/images/ekip2.jpg";
+  static const String ekip3 = "assets/images/ekip3.jpg";
+
+  static const String blog1 = "assets/images/blog1.png";
+  static const String blog2 = "assets/images/blog2.jpg";
+  static const String blog3 = "assets/images/blog3.jpg";
+
+  //Icons
+  static const String blackBack = "assets/icons/black-back.png";
+  static const String blackForward = "assets/icons/black-forward.png";
+  static const String blackSSS = "assets/icons/black-sss.png";
+  static const String downSSS = "assets/icons/down-sss.png";
+  static const String whiteSSS = "assets/icons/white-sss.png";
+  static const String purpleBack = "assets/icons/purple-back.png";
+  static const String purpleCross = "assets/icons/purple-cross.png";
+  static const String purpleFilter = "assets/icons/purple-filter.png";
+  static const String whiteBack = "assets/icons/white-back.png";
+  static const String whiteCross = "assets/icons/white-cross.png";
+  static const String whiteFilter = "assets/icons/white-back.png";
+  static const String whitePlus = "assets/icons/white-plus.png";
+  static const String ikWhiteDown = "assets/icons/ik-white-down.png";
+  static const String ikWhiteUp = "assets/icons/ik-white-up.png";
+
+  static const String calendarChoose = "assets/icons/calendar-choose.png";
+  static const String calendarClock = "assets/icons/calendar-clock.png";
+  static const String calendarEducation = "assets/icons/calendar-education.png";
+  static const String calendarProfile = "assets/icons/calendar-profile.png";
+  static const String close = "assets/icons/close.png";
+  static const String company = "assets/icons/company.png";
+  static const String graduationCap = "assets/icons/graduation-cap.png";
+  static const String menu = "assets/icons/menu.png";
+  static const String news = "assets/icons/news.png";
+  static const String share = "assets/icons/share.png";
+  static const String monthLeftArrowFill = "assets/icons/month-leftarrow-fill.png";
+  static const String monthRightArrowEmpty = "assets/icons/month-rightarrow-empty.png";
+  static const String monthWhiteLeftArrowFill = "assets/icons/month-white-leftarrow-fill.png";
+  static const String unread = "assets/icons/unread.png";
+
+  static const String facebook = "assets/icons/facebook.png";
+  static const String whiteFacebook = "assets/icons/white-facebook.png";
+  static const String blackX = "assets/icons/black-x.png";
+  static const String whiteX = "assets/icons/white-x.png";
+  static const String whatsapp = "assets/icons/whatsapp.png";
+}
