402aaa6da6f6b8ff455b8c97b4c161f2d7d7b475
Şule Duman <sduman_816@hotmail.com>
2024-05-30 23:39:50 +0300

    Update login_screen.dart

--- lib/screens/login_screen/login_screen.dart
+++ lib/screens/login_screen/login_screen.dart
@@ -27,6 +27,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Stack(
         children: [
           const LoginBackground(),
