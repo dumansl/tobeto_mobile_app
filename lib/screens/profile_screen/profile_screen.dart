4fd9db5ae90863eb419278738975be5f01d43dde
ekremtek <ekremtek@gmail.com>
2024-06-01 02:52:41 +0300

    01.06.2024Ekremprofil

--- lib/screens/profile_screen/profile_screen.dart
+++ lib/screens/profile_screen/profile_screen.dart
@@ -10,8 +10,7 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: TobetoColor.card.cream,
-        title: Text(TobetoText.profileAppBar,
-            style: TobetoTextStyle.poppins.headlinePurpleBold32),
+        title: Text(TobetoText.profileAppBar, style: TobetoTextStyle.poppins.headlinePurpleBold32),
         centerTitle: true,
         leading: IconButton(
           icon: Image.asset(ImagePath.purpleBack),
@@ -51,119 +50,388 @@ class ProfileScreen extends StatelessWidget {
                 icon: Image.asset(ImagePath.organizer),
               ),
             ),
-            LayoutBuilder(
-              builder: (BuildContext context, BoxConstraints constraints) {
-                return ConstrainedBox(
-                  constraints: const BoxConstraints(
-                    minHeight: 0,
-                    maxHeight: double.infinity,
+            MainTitleCard(
+              iconAndTexts: [
+                IconAndText(
+                  icon: ImagePath.user,
+                  text: TobetoText.profileName,
+                  value: "Ekrem TEK",
+                ),
+                IconAndText(
+                  icon: ImagePath.birthdate,
+                  text: TobetoText.profileBirthday,
+                  value: "19.01.1989",
+                ),
+                IconAndText(
+                  icon: ImagePath.mail,
+                  text: TobetoText.profileEmail,
+                  value: "ekremtek@gmail.com",
+                ),
+                IconAndText(
+                  icon: ImagePath.phone,
+                  text: TobetoText.profilePhoneNumber,
+                  value: "+90 (533) 669 75 68",
+                ),
+                IconAndText(
+                  icon: ImagePath.gender,
+                  text: TobetoText.profileGender,
+                  value: "Erkek",
+                ),
+                IconAndText(
+                  icon: ImagePath.soldier,
+                  text: TobetoText.profileMilitaryStuation,
+                  value: "Yapıldı",
+                ),
+                IconAndText(
+                  icon: ImagePath.disabled,
+                  text: TobetoText.profileDisableStuation,
+                  value: "Yok",
+                ),
+              ],
+            ),
+            TitleCard(
+              title: TobetoText.profileAboutMe,
+              content: Text(
+                'Elektronik ve Haberleşme Mühendisiyim. Mühendislik alanında farklı sektörlerde toplam 12 yıl deneyimim bulunmaktadır.',
+                style: TobetoTextStyle.poppins.bodyGrayDarkNormal16,
+              ),
+            ),
+            TitleCard(
+              title: TobetoText.profileMySkills,
+              content: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TwoLineCard(
+                    line1: '',
+                    line2: 'Takım Yönetimi',
                   ),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        width: double.infinity,
-                        child: Card(
-                          color: TobetoColor.card.white,
-                          margin: const EdgeInsets.only(bottom: 5),
-                          child: Padding(
-                            padding: EdgeInsets.all(ScreenPadding.padding16px),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                iconAndText(
-                                    ImagePath.user, TobetoText.profileName),
-                                iconAndText(ImagePath.birthdate,
-                                    TobetoText.profileBirthday),
-                                iconAndText(
-                                    ImagePath.mail, TobetoText.profileEmail),
-                                iconAndText(ImagePath.phone,
-                                    TobetoText.profilePhoneNumber),
-                                iconAndText(
-                                    ImagePath.gender, TobetoText.profileGender),
-                                iconAndText(ImagePath.soldier,
-                                    TobetoText.profileMilitaryStuation),
-                                iconAndText(ImagePath.disabled,
-                                    TobetoText.profileDisableStuation),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                      titleCards(TobetoText.profileAboutMe),
-                      titleCards(
-                        TobetoText.profileMySkills,
-                      ),
-                      titleCards(TobetoText.profileLanguages),
-                      titleCards(TobetoText.profileMyCertificate),
-                      titleCards(TobetoText.profileProjectAwards),
-                      titleCards(TobetoText.profileWorkExperiences),
-                      titleCards(TobetoText.profileInternship),
-                      titleCards(TobetoText.profileCommunity),
-                      titleCards(TobetoText.profileEducations),
-                      titleCards(TobetoText.profileTobetoWorkSuccess,
-                          onPressed: () {
-                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                            content: Text(
-                                'Tobeto Work Success Başla Butonuna Tıklandı!')));
-                      }),
-                      titleCards(TobetoText.profileTobetoTests),
-                      titleCards(TobetoText.profileTobetoEducations),
-                    ],
+                  TwoLineCard(
+                    line1: '',
+                    line2: 'Mobil Geliştirici',
                   ),
+                  TwoLineCard(
+                    line1: '',
+                    line2: 'Çok Görevli Çalışma',
+                  ),
+                ],
+              ),
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return const PopUpDialog();
+                  },
                 );
               },
+              icon: ImagePath.eye,
+            ),
+            TitleCard(
+              title: TobetoText.profileLanguages,
+              content: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TwoLineCard(
+                    line2: 'Türkçe',
+                    line1: 'Ana Dil',
+                  ),
+                  TwoLineCard(
+                    line2: 'İngilizce',
+                    line1: 'Orta Seviye (B1-B2)',
+                  ),
+                ],
+              ),
+            ),
+            TitleCard(
+              title: TobetoText.profileMyCertificate,
+              content: Text(
+                'Henüz bir sertifika eklemediniz.',
+                style: TobetoTextStyle.poppins.bodyGrayDarkNormal16,
+              ),
+            ),
+            TitleCard(
+              title: TobetoText.profileProjectAwards,
+              content: Text(
+                'Henüz bir proje veya ödül eklemediniz.',
+                style: TobetoTextStyle.poppins.bodyGrayDarkNormal16,
+              ),
+            ),
+            TitleCard(
+              title: TobetoText.profileWorkExperiences,
+              content: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TwoLineCard(
+                    line1: 'Temmuz 2013 - Ekim 2015',
+                    line2: 'Akbank - Kıdemli Mühendis',
+                  ),
+                ],
+              ),
+            ),
+            TitleCard(
+              title: TobetoText.profileInternship,
+              content: Text(
+                'Henüz bir staj veya gönüllü çalışma deneyimi eklemediniz.',
+                style: TobetoTextStyle.poppins.bodyGrayDarkNormal16,
+              ),
+            ),
+            TitleCard(
+              title: TobetoText.profileCommunity,
+              content: Text(
+                'Henüz bir topluluk eklemediniz.',
+                style: TobetoTextStyle.poppins.bodyGrayDarkNormal16,
+              ),
+            ),
+            TitleCard(
+              title: TobetoText.profileEducations,
+              content: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TwoLineCard(
+                    line1: 'Ocak 2009 - Haziran 2013',
+                    line2: 'Kocaeli - ElohabMüh',
+                  ),
+                ],
+              ),
+            ),
+            TitleCard(
+              title: TobetoText.profileTobetoWorkSuccess,
+              content: Column(
+                children: [
+                  Text(
+                    'Henüz Tobeto Başarı Testi¨ne girmediniz.',
+                    style: TobetoTextStyle.poppins.bodyGrayDarkNormal16,
+                    textAlign: TextAlign.left,
+                  ),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                              content: Text('Tobeto Work Success Başla Butonuna Tıklandı!')),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: TobetoColor.purple,
+                        foregroundColor: TobetoColor.text.white,
+                      ),
+                      child: Text('Başla', style: TobetoTextStyle.poppins.captionWhiteSemiBold15),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            TitleCard(
+              title: TobetoText.profileTobetoTests,
+              content: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TwoLineCard(
+                    line1: '31.05.2024',
+                    line2: 'Full Stack - Not: 60',
+                  ),
+                ],
+              ),
+            ),
+            TitleCard(
+              title: TobetoText.profileTobetoEducations,
+              content: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TwoLineCard(
+                    line1: '22.09.2023 - Devam Ediyor...',
+                    line2: 'Hoşgeldin Buluşması - 4',
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
     );
   }
+}
 
-  Widget iconAndText(String iconPath, String text) {
+class MainTitleCard extends StatelessWidget {
+  final List<IconAndText> iconAndTexts;
+
+  const MainTitleCard({super.key, required this.iconAndTexts});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) => ConstrainedBox(
+              constraints: const BoxConstraints(
+                minHeight: 0,
+                maxHeight: double.infinity,
+              ),
+              child: Column(children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: Card(
+                    color: TobetoColor.card.white,
+                    margin: const EdgeInsets.only(bottom: 5),
+                    child: Padding(
+                      padding: EdgeInsets.all(ScreenPadding.padding16px),
+                      child: Column(
+                        children: iconAndTexts,
+                      ),
+                    ),
+                  ),
+                )
+              ]),
+            ));
+  }
+}
+
+class IconAndText extends StatelessWidget {
+  final String icon;
+  final String text;
+  final String value;
+
+  const IconAndText({
+    super.key,
+    required this.icon,
+    required this.text,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return Row(
       children: [
-        Image.asset(iconPath, width: 55, height: 55),
-        const SizedBox(width: 8),
-        Text(text, style: TobetoTextStyle.poppins.bodyGrayLightLight16),
+        Image.asset(icon),
+        const SizedBox(width: 15),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(text, style: TobetoTextStyle.poppins.bodyGrayDarkSemiBold16),
+            Text(value, style: TobetoTextStyle.poppins.captionGrayBold18),
+          ],
+        ),
       ],
     );
   }
 }
 
-Widget titleCards(String title, {VoidCallback? onPressed}) {
-  return SizedBox(
-    width: double.infinity,
-    child: Card(
-      color: TobetoColor.card.white,
-      margin: const EdgeInsets.only(bottom: 5),
-      child: Padding(
-        padding: EdgeInsets.all(ScreenPadding.padding16px),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(title, style: TobetoTextStyle.poppins.bodyBlackBold16),
-                if (onPressed != null)
-                  ElevatedButton(
-                    onPressed: onPressed,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: TobetoColor.purple, // Arka plan rengi
-                      foregroundColor: TobetoColor.text.white, // Metin rengi
+class TitleCard extends StatelessWidget {
+  final String title;
+  final Widget content;
+  final Function()? onTap;
+  final String? icon;
+
+  const TitleCard({
+    super.key,
+    required this.title,
+    required this.content,
+    this.onTap,
+    this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Card(
+        color: TobetoColor.card.white,
+        margin: const EdgeInsets.only(bottom: 5),
+        child: Padding(
+          padding: EdgeInsets.all(ScreenPadding.padding16px),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(title, style: TobetoTextStyle.poppins.bodyBlackBold16),
+                  if (icon != null)
+                    InkWell(
+                      onTap: onTap,
+                      child: Image.asset(icon!),
                     ),
-                    child: Text('Başla',
-                        style: TobetoTextStyle.poppins.captionWhiteSemiBold15),
-                  ),
-              ],
-            ),
-            const Divider(
-              thickness: 0.7,
-              color: TobetoColor.purple,
-            )
-          ],
+                ],
+              ),
+              const Divider(
+                thickness: 0.7,
+                color: TobetoColor.purple,
+              ),
+              content,
+            ],
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class TwoLineCard extends StatelessWidget {
+  final String? line1;
+  final String line2;
+
+  const TwoLineCard({super.key, this.line1, required this.line2});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+        color: TobetoColor.card.white,
+        margin: const EdgeInsets.only(bottom: 5),
+        child: Padding(
+          padding: EdgeInsets.all(ScreenPadding.padding16px),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(line1!, style: TobetoTextStyle.poppins.captionGrayNormal12),
+              Text(line2, style: TobetoTextStyle.poppins.bodyGrayDarkSemiBold16),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PopUpDialog extends StatelessWidget {
+  const PopUpDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: TobetoColor.card.cream,
+      title: Text(TobetoText.profileMySkills),
+      content: const Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          DialogCard(skill: 'Takım Yönetimi'),
+          DialogCard(skill: 'Mobil Geliştiricilik'),
+          DialogCard(skill: 'Çok Görevli Çalışma'),
+          DialogCard(skill: 'Aktif Dinleme'),
+          DialogCard(skill: 'İletişim'),
+        ],
+      ),
+    );
+  }
+}
+
+class DialogCard extends StatelessWidget {
+  final String skill;
+
+  const DialogCard({super.key, required this.skill});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+        color: TobetoColor.card.white,
+        margin: const EdgeInsets.only(bottom: 5),
+        child: Padding(
+          padding: EdgeInsets.all(ScreenPadding.padding16px),
+          child: Text(skill, style: TobetoTextStyle.poppins.bodyGrayDarkSemiBold16),
+        ),
+      ),
+    );
+  }
 }
