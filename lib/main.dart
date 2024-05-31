64fd1abdbd1717717c6a98566ff37f57282f7153
Şule Duman <sduman_816@hotmail.com>
2024-06-01 00:12:42 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tobeto_mobile_app/blocs/course_bloc/course_bloc.dart';
 import 'package:tobeto_mobile_app/blocs/theme_bloc/theme_bloc.dart';
 import 'package:tobeto_mobile_app/screens/screens.dart';
+import 'package:tobeto_mobile_app/utils/themes/theme.dart';
 import 'blocs/connect_bloc/connection_bloc.dart';
 import 'firebase_options.dart';
 
@@ -26,9 +27,16 @@ class TobetoMobileApp extends StatelessWidget {
         BlocProvider<CourseBloc>(create: (context) => CourseBloc()),
         BlocProvider<ThemeBloc>(create: (context) => ThemeBloc()),
       ],
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: RegisterScreen(),
+      child: BlocBuilder<ThemeBloc, ThemeMode>(
+        builder: (context, state) {
+          return MaterialApp(
+            theme: lightTheme,
+            themeMode: state,
+            darkTheme: darkTheme,
+            debugShowCheckedModeBanner: false,
+            home: const RegisterScreen(), // Kendinize göre değiştirin
+          );
+        },
       ),
     );
   }
