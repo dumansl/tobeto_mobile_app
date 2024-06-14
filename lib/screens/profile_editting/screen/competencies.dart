6feaee75890bb40a436dc0767450ee4d23148160
Enes YILDIZ <yildizenes.4207@hotmail.com>
2024-06-14 14:05:43 +0300

    competencies changes

--- lib/screens/profile_editting/screen/competencies.dart
+++ lib/screens/profile_editting/screen/competencies.dart
@@ -1,36 +1,101 @@
-// import 'package:flutter/material.dart';
-// import 'package:tobeto_mobile_app/screens/profile_editting/widgets/custom_elevated_button.dart';
-// import 'package:tobeto_mobile_app/screens/profile_editting/widgets/custom_mini_card.dart';
-// import 'package:tobeto_mobile_app/screens/profile_editting/widgets/custom_textfield.dart';
-// import 'package:tobeto_mobile_app/screens/profile_editting/widgets/input_text.dart';
-// import 'package:tobeto_mobile_app/utils/constant/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:tobeto_mobile_app/screens/profile_editting/widgets/custom_elevated_button.dart';
+import 'package:tobeto_mobile_app/screens/profile_editting/widgets/custom_mini_card.dart';
+import 'package:tobeto_mobile_app/screens/profile_editting/widgets/custom_textfield.dart';
+import 'package:tobeto_mobile_app/screens/profile_editting/widgets/input_text.dart';
+import 'package:tobeto_mobile_app/utils/constant/constants.dart';
 
-// class Competencies extends StatefulWidget {
-//   const Competencies({super.key});
+class Competencies extends StatefulWidget {
+  const Competencies({super.key});
 
-//   @override
-//   State<Competencies> createState() => _CompetenciesState();
-// }
+  @override
+  State<Competencies> createState() => _CompetenciesState();
+}
 
-// class _CompetenciesState extends State<Competencies> {
-//   final _formKey = GlobalKey<FormState>();
-//   @override
-//   Widget build(BuildContext context) {
-//     return Form(
-//         key: _formKey,
-//         child: ListView(
-//           children: [
-//             InputText(child: CustomTextField(title: TobetoText.profileEditSkill)),
-//             CustomElevatedButton(
-//               text: "Ekle",
-//               onPressed: () {},
-//             ),
-//             InputText(child: CustomMiniCard(onpressed: () {}, title: 'Aktif Öğrenme')),
-//             InputText(child: CustomMiniCard(onpressed: () {}, title: 'Algoritma')),
-//             InputText(child: CustomMiniCard(onpressed: () {}, title: 'Yönetim')),
-//             InputText(child: CustomMiniCard(onpressed: () {}, title: 'Takım Çalışması')),
-//             InputText(child: CustomMiniCard(onpressed: () {}, title: 'Kodlama')),
-//           ],
-//         ));
-//   }
-// }
+final TextEditingController skillController = TextEditingController();
+
+class _CompetenciesState extends State<Competencies> {
+  final _formKey = GlobalKey<FormState>();
+  List<String> skills = [];
+  final userId = FirebaseAuth.instance.currentUser!.uid;
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSkills();
+  }
+
+  Future<void> _loadSkills() async {
+    try {
+      DocumentSnapshot doc = await db.collection('users').doc(userId).get();
+      if (doc.exists) {
+        var data = doc.data() as Map<String, dynamic>;
+        setState(() {
+          skills = List<String>.from(data['skill'] ?? []);
+        });
+      }
+    } catch (e) {
+      print("Failed to load skills: $e");
+    }
+  }
+
+  Future<void> _addSkill(String skill) async {
+    setState(() {
+      skills.add(skill);
+    });
+    await db.collection('users').doc(userId).update({
+      'skill': FieldValue.arrayUnion([skill])
+    });
+  }
+
+  Future<void> _removeSkill(String skill) async {
+    setState(() {
+      skills.remove(skill);
+    });
+    await db.collection('users').doc(userId).update({
+      'skill': FieldValue.arrayRemove([skill])
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: ListView(
+        children: [
+          InputText(
+            child: CustomTextField(
+              title: TobetoText.profileEditSkill,
+              onSaved: (newValue) {
+                skillController.text = newValue ?? skillController.text;
+              },
+              controller: skillController,
+            ),
+          ),
+          CustomElevatedButton(
+            text: "Ekle",
+            onPressed: () {
+              if (skillController.text.isNotEmpty) {
+                _addSkill(skillController.text);
+                skillController.clear();
+              }
+            },
+          ),
+          ...skills.map((skill) {
+            return InputText(
+              child: CustomMiniCard(
+                onpressed: () {
+                  _removeSkill(skill);
+                },
+                title: skill,
+              ),
+            );
+          }).toList(),
+        ],
+      ),
+    );
+  }
+}
