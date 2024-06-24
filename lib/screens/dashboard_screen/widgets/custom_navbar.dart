163484bf84d18796684102c65f40fc5a8c52026c
Emrah IŞIK <ssemrah@outlook.com>
2024-06-24 18:01:23 +0300

    emrah

--- lib/screens/dashboard_screen/widgets/custom_navbar.dart
+++ lib/screens/dashboard_screen/widgets/custom_navbar.dart
@@ -16,7 +16,8 @@ class CustomNavBar extends StatelessWidget {
   final NavBarConfig navBarConfig;
   final NavBarDecoration navBarDecoration;
 
-  Widget _buildItem(BuildContext context, ItemConfig item, bool isSelected) => Column(
+  Widget _buildItem(BuildContext context, ItemConfig item, bool isSelected) =>
+      Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Expanded(
@@ -83,12 +84,16 @@ class CustomNavBar extends StatelessWidget {
           children: [
             SizedBox(height: IconSize.size25px),
             DecoratedNavBar(
-              decoration: NavBarDecoration(boxShadow: [
-                BoxShadow(
-                  color: TobetoColor.card.shadowColor,
-                  blurRadius: SizeRadius.radius10px,
-                ),
-              ], borderRadius: BorderRadius.vertical(top: Radius.circular(SizeRadius.radius20px))),
+              decoration: NavBarDecoration(
+                  color: Theme.of(context).colorScheme.onSurface, //dasbord renk
+                  boxShadow: [
+                    BoxShadow(
+                      color: TobetoColor.card.shadowColor,
+                      blurRadius: SizeRadius.radius10px,
+                    ),
+                  ],
+                  borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(SizeRadius.radius20px))),
               // ignore: deprecated_member_use
               filter: navBarConfig.selectedItem.filter,
               height: IconSize.size60px,
