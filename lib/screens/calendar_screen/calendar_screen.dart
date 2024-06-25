cb9886fd1a9c7854beb96a30b38b7a1024d8bf76
Enes YILDIZ <yildizenes.4207@hotmail.com>
2024-06-25 15:54:31 +0300

    calendar changes

--- lib/screens/calendar_screen/calendar_screen.dart
+++ lib/screens/calendar_screen/calendar_screen.dart
@@ -6,6 +6,8 @@ import 'package:tobeto_mobile_app/blocs/calendar_bloc/calendar_event.dart';
 import 'package:tobeto_mobile_app/blocs/calendar_bloc/calendar_state.dart';
 import 'package:tobeto_mobile_app/model/calendar_model.dart';
 import 'package:tobeto_mobile_app/services/calendar_service.dart';
+import 'package:tobeto_mobile_app/utils/constant/colors.dart';
+import 'package:tobeto_mobile_app/utils/constant/constants.dart';
 
 class CalendarScreen extends StatelessWidget {
   @override
@@ -30,31 +32,31 @@ class CalendarScreen extends StatelessWidget {
               print('CalendarLoading state');
               return const Center(child: CircularProgressIndicator());
             } else if (state is CalendarLoaded) {
-              print(
-                  'CalendarLoaded state with ${state.lessons.length} lessons');
+              print('CalendarLoaded state with ${state.lessons.length} lessons');
               return Column(
                 children: [
                   TableCalendar(
+                    headerStyle: const HeaderStyle(formatButtonVisible: false),
+                    calendarStyle: CalendarStyle(
+                        todayDecoration: ShapeDecoration(
+                            color: const Color.fromARGB(255, 0, 221, 7).withOpacity(0.4), shape: const CircleBorder()),
+                        selectedDecoration: const ShapeDecoration(color: TobetoColor.purple, shape: CircleBorder())),
                     firstDay: DateTime.utc(2020, 10, 16),
                     lastDay: DateTime.utc(2030, 3, 14),
                     focusedDay: state.selectedDate,
-                    selectedDayPredicate: (day) =>
-                        isSameDay(state.selectedDate, day),
+                    selectedDayPredicate: (day) => isSameDay(state.selectedDate, day),
                     onDaySelected: (selectedDay, focusedDay) {
                       context.read<CalendarBloc>().add(SelectDate(selectedDay));
                     },
                     calendarBuilders: CalendarBuilders(
                       markerBuilder: (context, date, events) {
-                        List<Lesson> lessons = state.lessons
-                            .where((lesson) => isSameDay(lesson.date, date))
-                            .toList();
+                        List<Lesson> lessons = state.lessons.where((lesson) => isSameDay(lesson.date, date)).toList();
                         if (lessons.isNotEmpty) {
                           return Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: lessons
                                 .map((lesson) => Container(
-                                      margin: const EdgeInsets.symmetric(
-                                          horizontal: 1.5),
+                                      margin: const EdgeInsets.symmetric(horizontal: 1.5),
                                       width: 5.0,
                                       height: 5.0,
                                       decoration: BoxDecoration(
@@ -71,22 +73,49 @@ class CalendarScreen extends StatelessWidget {
                   ),
                   Expanded(
                     child: ListView.builder(
-                      itemCount: state.lessons
-                          .where((lesson) =>
-                              isSameDay(lesson.date, state.selectedDate))
-                          .length,
+                      itemCount: state.lessons.where((lesson) => isSameDay(lesson.date, state.selectedDate)).length,
                       itemBuilder: (context, index) {
-                        Lesson lesson = state.lessons
-                            .where((lesson) =>
-                                isSameDay(lesson.date, state.selectedDate))
-                            .toList()[index];
+                        Lesson lesson =
+                            state.lessons.where((lesson) => isSameDay(lesson.date, state.selectedDate)).toList()[index];
                         return Card(
-                          color: Color(lesson.color),
+                          color: Color(lesson.color).withOpacity(0.5),
                           child: ListTile(
-                            title: Text(lesson.time),
-                            subtitle: Text(lesson.instructor),
-                            leading: Text(lesson.title),
-                          ),
+                              title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  const SizedBox(
+                                      child: Icon(
+                                    Icons.access_alarm_rounded,
+                                    size: 30,
+                                  )),
+                                  const SizedBox(width: 15),
+                                  Text(lesson.time),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  const Icon(
+                                    Icons.menu_book_outlined,
+                                    size: 30,
+                                  ),
+                                  const SizedBox(width: 15),
+                                  Text(lesson.instructor),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  const Icon(
+                                    Icons.person_rounded,
+                                    size: 30,
+                                  ),
+                                  const SizedBox(width: 15),
+                                  Text(lesson.title),
+                                ],
+                              )
+                            ],
+                          )),
                         );
                       },
                     ),
