e4dc2032ce58698422f7a0d972ad6dca9e88fb50
Şule Duman <sduman_816@hotmail.com>
2024-06-27 22:55:38 +0300

    Updated

--- lib/screens/dashboard_screen/widgets/fixed_appbar.dart
+++ lib/screens/dashboard_screen/widgets/fixed_appbar.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tobeto_mobile_app/blocs/profile_photo_bloc/profile_photo_bloc.dart';
 import 'package:tobeto_mobile_app/blocs/profile_photo_bloc/profile_photo_state.dart';
-import 'package:tobeto_mobile_app/utils/constant/colors.dart';
-import 'package:tobeto_mobile_app/utils/constant/image_string.dart';
-import 'package:tobeto_mobile_app/utils/constant/sizes.dart';
+import 'package:tobeto_mobile_app/screens/login_screen/login_screen.dart';
+import 'package:tobeto_mobile_app/utils/constant/constants.dart';
+import 'package:tobeto_mobile_app/blocs/auth_bloc/auth_bloc.dart';
+import 'package:tobeto_mobile_app/blocs/auth_bloc/auth_event.dart';
+import 'package:tobeto_mobile_app/blocs/auth_bloc/auth_state.dart';
 
 class FixedAppbar extends StatelessWidget implements PreferredSizeWidget {
   const FixedAppbar({
@@ -20,78 +22,114 @@ class FixedAppbar extends StatelessWidget implements PreferredSizeWidget {
   final Widget? title;
   final bool isLeading;
   final bool isTobetoScreen;
+
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      iconTheme: const IconThemeData(
-        color: TobetoColor.purple,
-      ),
-      toolbarHeight: ScreenUtil.getHeight(context) * 0.090,
-      title: isTobetoScreen
-          ? Padding(
-              padding: EdgeInsets.all(ScreenPadding.padding24px),
-              child: Image.asset(ImagePath.greyTobeto),
-            )
-          : title,
-      centerTitle: true,
-      leading: isLeading
-          ? IconButton(
-              icon: Icon(
-                Icons.arrow_back_rounded,
-                color: TobetoColor.purple,
-                size: IconSize.size35px,
-              ),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            )
-          : null,
-      actions: [
-        BlocBuilder<ProfilePhotoBloc, ProfilePhotoState>(
-            builder: (context, state) {
-          if (state is ProfilePhotoLoading) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is ProfilePhotoLoaded) {
-            return Padding(
-              padding: EdgeInsets.only(right: ScreenPadding.padding12px),
-              child: Stack(alignment: Alignment.center, children: [
-                Container(
-                  width: ScreenUtil.getWidth(context) * 0.155,
-                  height: ScreenUtil.getHeight(context) * 0.155,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    gradient: LinearGradient(
-                      colors: [
-                        TobetoColor.purple,
-                        TobetoColor.rainbow.lineargreen,
-                        TobetoColor.rainbow.linaergreenv2,
-                        TobetoColor.rainbow.linearyellow,
-                        TobetoColor.purple,
-                      ],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is NotAuthenticated) {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const LoginScreen(),
+            ),
+            (route) => false,
+          );
+        }
+      },
+      child: AppBar(
+        iconTheme: const IconThemeData(
+          color: TobetoColor.purple,
+        ),
+        toolbarHeight: ScreenUtil.getHeight(context) * 0.090,
+        title: isTobetoScreen
+            ? Padding(
+                padding: EdgeInsets.all(ScreenPadding.padding24px),
+                child: Image.asset(ImagePath.greyTobeto),
+              )
+            : title,
+        centerTitle: true,
+        leading: isLeading
+            ? IconButton(
+                icon: Icon(
+                  Icons.arrow_back_rounded,
+                  color: TobetoColor.purple,
+                  size: IconSize.size35px,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            : null,
+        actions: [
+          BlocBuilder<ProfilePhotoBloc, ProfilePhotoState>(
+              builder: (context, state) {
+            if (state is ProfilePhotoLoading) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is ProfilePhotoLoaded) {
+              return Padding(
+                padding: EdgeInsets.only(right: ScreenPadding.padding12px),
+                child: Stack(alignment: Alignment.center, children: [
+                  Container(
+                    width: ScreenUtil.getWidth(context) * 0.155,
+                    height: ScreenUtil.getHeight(context) * 0.155,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      gradient: LinearGradient(
+                        colors: [
+                          TobetoColor.purple,
+                          TobetoColor.rainbow.lineargreen,
+                          TobetoColor.rainbow.linaergreenv2,
+                          TobetoColor.rainbow.linearyellow,
+                          TobetoColor.purple,
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
                     ),
                   ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: CircleAvatar(
-                    radius: ScreenUtil.getWidth(context) * 0.075,
-                    foregroundImage: state.imageUrl.isNotEmpty
-                        ? NetworkImage(state.imageUrl)
-                        : const AssetImage(ImagePath.defaultProfilePhoto),
+                  PopupMenuButton<String>(
+                    color: TobetoColor.card.white,
+                    onSelected: (value) {
+                      if (value == 'logout') {
+                        context.read<AuthBloc>().add(const LogoutEvent());
+                      }
+                    },
+                    itemBuilder: (BuildContext context) {
+                      return [
+                        PopupMenuItem<String>(
+                          value: 'logout',
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.exit_to_app_rounded,
+                                size: IconSize.size25px,
+                              ),
+                              const SizedBox(width: 8),
+                              Text(TobetoText.exit),
+                            ],
+                          ),
+                        ),
+                      ];
+                    },
+                    icon: CircleAvatar(
+                      radius: ScreenUtil.getWidth(context) * 0.075,
+                      foregroundImage: state.imageUrl.isNotEmpty
+                          ? NetworkImage(state.imageUrl)
+                          : const AssetImage(ImagePath.defaultProfilePhoto),
+                    ),
                   ),
-                ),
-              ]),
-            );
-          } else if (state is ProfilePhotoError) {
-            return Center(child: Text('Error: ${state.message}'));
-          } else {
-            return const Center(child: Text('No profile photo loaded.'));
-          }
-        })
-      ],
-      bottom: bottom,
+                ]),
+              );
+            } else if (state is ProfilePhotoError) {
+              return Center(child: Text('Error: ${state.message}'));
+            } else {
+              return const Center(child: Text('No profile photo loaded.'));
+            }
+          })
+        ],
+        bottom: bottom,
+      ),
     );
   }
 
